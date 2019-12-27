1431398efec5ac096d56f7e24039473471b2559d
Ajay Kumar <imajkumar@hotmail.com>
2019-12-27 11:45:08 +0530

    added

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './style/main.less';
 
 class Welcome extends React.Component {
-    render () {
-        return (
-           <div>
-               <h1>React demo</h1>              
-           </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>React demo</h1>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<Welcome />, document.getElementById('root'));
